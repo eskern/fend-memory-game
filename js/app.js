66e080ffdbf06e85d627501ae3511a3f7b359cf2
Emily Sarah Kern <emily.kern28@gmail.com>
2018-10-25 17:40:08 -0400

    Big commit; got game logic working

--- js/app.js
+++ js/app.js
@@ -1,7 +1,77 @@
 /*
- * Create a list that holds all of your cards
+ * Author: Emily Kern
+ * Resources: Matthew Cranford'd walkthrough,
+ *            w3schools.com to recall syntax and properties
  */
 
+var moves = 0;
+var faceUp = [];
+
+/*
+ * Create a list that holds all of your cards!
+ * My understanding: non-matched cards have .open and .show classes
+ * Matching cards have the class .match
+ * We can toggle which classes are on/off when a card is clicked
+ * and if there is indeed a match
+ */
+const deck = document.querySelector('.deck');
+deck.addEventListener('click', function(){
+  const clickedCard = event.target;
+  if(isValid(clickedCard)){
+    flipCard(clickedCard);
+    add2Flipped(clickedCard);
+    if(faceUp.length === 2){
+      if(checkMatch(faceUp)){
+        faceUp[0].classList.toggle('match');
+        faceUp[1].classList.toggle('match');
+        faceUp = [];
+      }
+      else{
+        setTimeout(function flipOver(){
+          flipCard(faceUp[0]);
+          flipCard(faceUp[1]);
+          faceUp = [];
+        }, 1000);
+      }
+      moves++;
+    }
+  }
+});
+
+/*
+ * Logic: If the clicked target is a card,
+ * and there aren't already two cards face up,
+ * and the clicked card isn't already face up in any capacity
+ */
+function isValid(clicked){
+  return(clicked.classList.contains('card') &&
+  faceUp.length < 2 && !faceUp.includes(clicked) &&
+  !clicked.classList.contains('match'));
+}
+
+/*
+ * Flips card over
+ */
+function flipCard(toFlip){
+  // Note: no need for '.' because this concerns classes, anyway
+  toFlip.classList.toggle('open');
+  toFlip.classList.toggle('show');
+}
+
+/*
+ * Add clicked card to the faceUp array
+ */
+function add2Flipped(clickedCard){
+  faceUp.push(clickedCard);
+}
+
+/*
+ * Checks if the selected cards match
+ */
+function checkMatch(faceUp){
+  return(faceUp[0].firstElementChild.className ===
+    faceUp[1].firstElementChild.className);
+}
 
 /*
  * Display the cards on the page
@@ -24,7 +94,6 @@ function shuffle(array) {
 
     return array;
 }
-
 
 /*
  * set up the event listener for a card. If a card is clicked:
